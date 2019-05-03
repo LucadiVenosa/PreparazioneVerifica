b836ff291681d034e25576bb804f1f680e81e74f
LucadiVenosa <di.venosa.luca@iisgalvanimi.edu.it>
2019-05-03 07:45:36 +0000

    Calcolatrice

--- Calcolatrice/src/app/app.component.ts
+++ Calcolatrice/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calcolatrice';
+
+   MyForm : FormGroup;
+   risultato : Number;
+
+   constructor(fb: FormBuilder) {
+     this.MyForm = fb.group({'numero1' : ['0', Validators.required], 'numero2' : ['0', Validators.required]})
+   } 
+
+     somma() {
+   this.risultato = Number(this.MyForm.controls['numero1'].value) + Number(this.MyForm.controls['numero2'].value);
+   return false;
+  }
+
+  sottrazione() {
+   this.risultato = Number(this.MyForm.controls['numero1'].value) - Number(this.MyForm.controls['numero2'].value);
+   return false;
+  }
+
+  moltiplicazione() {
+   this.risultato = Number(this.MyForm.controls['numero1'].value) * Number(this.MyForm.controls['numero2'].value);
+   return false;
+  }
+  
+  divisione() {
+   this.risultato = Number(this.MyForm.controls['numero1'].value) / Number(this.MyForm.controls['numero2'].value);
+   return false;
+  }
 }
